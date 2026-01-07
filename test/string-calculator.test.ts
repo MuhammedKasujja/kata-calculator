f47b0bef5261c96a0b56d95bd1d4c24a5810447c
MuhammedKasujja <al.kasmud.2@gmail.com>
2026-01-07 20:03:10 +0300

    test(refactor): using same calculator instance for all test cases

--- test/string-calculator.test.ts
+++ test/string-calculator.test.ts
@@ -1,21 +1,24 @@
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 import { StringCalculator } from "../src/string-calculator";
 
 describe("String Calculator Tests", () => {
+  let calc: StringCalculator;
+
+  beforeEach(() => {
+    calc = new StringCalculator();
+  });
+
   test("should return zero for an empty string", () => {
-    const calc = new StringCalculator()
     expect(calc.add("")).toBe(0);
     expect(calc.add("   ")).toBe(0);
   });
 
   test("should return the number itself when only one number given", () => {
-    const calc = new StringCalculator()
     expect(calc.add("1")).toBe(1);
     expect(calc.add("60")).toBe(60);
   });
-  
+
   test("should return the sum of the given two numbers", () => {
-    const calc = new StringCalculator()
     expect(calc.add("1,5")).toBe(6);
   });
 });
