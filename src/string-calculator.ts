a1dfcc1812aacd9794f26e512a396547698944b9
MuhammedKasujja <al.kasmud.2@gmail.com>
2026-01-07 19:43:04 +0300

    feat: string calculator - handle empty input

---
+++ src/string-calculator.ts
@@ -0,0 +1,5 @@
+export class StringCalculator {
+  add(numbers: string) {
+    if(numbers === "") return 0
+  }
+}
---
+++ test/string-calculator.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from "vitest";
+import { StringCalculator } from "../src/string-calculator";
+
+describe("String Calculator Tests", () => {
+  test("should return zero for an empty string", () => {
+    const calc = new StringCalculator()
+    expect(calc.add("")).toBe(0);
+  });
+});
